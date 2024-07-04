179617ad2fa6b4be58bc5bc67d850b0fa6e56004
VKTRenokh <viktorenokh@gmail.com>
2024-07-04 14:35:30 +0300

    feat: add search components

---
+++ youtube/src/app/search/search-item/search-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yt-search-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-item.component.html',
+  styleUrl: './search-item.component.scss',
+})
+export class SearchItemComponent {}
--- youtube/src/main.ts
+++ youtube/src/main.ts
@@ -5,7 +5,3 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
-
-console.log('hello');
-
-console.log();
