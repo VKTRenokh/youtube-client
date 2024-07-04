08c7edabb62c1c665f5f549edbdca494bd6364fa
VKTRenokh <viktorenokh@gmail.com>
2024-07-04 16:54:57 +0300

    feat: add interfaces

---
+++ youtube/src/app/search/interfaces/search-response.interface.ts
@@ -0,0 +1,13 @@
+import { SearchItem } from './search-item.interface';
+
+export interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
+
+export interface SearchResponse {
+  kind: string;
+  etag: string;
+  pageInfo: PageInfo;
+  items: SearchItem[];
+}
