ed35547dae8253fec7930c03248d21ffa76ea186
VKTRenokh <viktorenokh@gmail.com>
2024-07-12 11:01:04 +0300

    feat: add more button functionality

--- youtube/src/app/youtube/components/search-item/search-item.component.ts
+++ youtube/src/app/youtube/components/search-item/search-item.component.ts
@@ -2,12 +2,14 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  inject,
   input,
 } from '@angular/core'
 import { NgOptimizedImage } from '@angular/common'
 import { ButtonComponent } from '../../../shared/components/button/button.component'
 import { ColoredBorderDirective } from '../../directives/colored-border.directive'
 import { VideoItem } from '../../models/response.model'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'yt-search-item',
@@ -22,10 +24,17 @@ import { VideoItem } from '../../models/response.model'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchItemComponent {
+  public router = inject(Router)
+
   public item = input.required<VideoItem>()
 
   public snippet = computed(() => this.item().snippet)
   public smallThumbnail = computed(
     () => this.snippet().thumbnails.default,
   )
+
+  public navigateToDetailedPage() {
+    console.log('navigate')
+    this.router.navigate([this.item().id])
+  }
 }
