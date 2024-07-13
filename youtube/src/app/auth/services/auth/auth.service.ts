65c85cffd22bc9187de9d54ef96c3b7eff1797cb
VKTRenokh <viktorenokh@gmail.com>
2024-07-13 16:11:29 +0300

    refactor: make storage service better

--- youtube/src/app/auth/services/auth/auth.service.ts
+++ youtube/src/app/auth/services/auth/auth.service.ts
@@ -1,35 +1,32 @@
 import { Injectable, inject, signal } from '@angular/core'
 import { generateToken } from '../../utils/generate-token'
-import { LocalStorageService } from '../../../core/services/local-storage/local-storage.service'
 import { LOGGER_TOKEN } from '../../../core/providers/logger/logger.provider'
+import { StorageService } from '../../../core/services/storage/storage.service'
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly authKey = 'fakeJWT'
-  private localStorageService = inject(LocalStorageService)
   private logger = inject(LOGGER_TOKEN)
+  private storageService = inject(StorageService)
 
   private _isLogined = signal(
-    !!this.localStorageService.get(this.authKey),
+    !!this.storageService.get(this.authKey),
   )
   public isLogined = this._isLogined.asReadonly()
 
   public login() {
     this._isLogined.set(true)
 
-    this.localStorageService.set(
-      this.authKey,
-      generateToken(),
-    )
+    this.storageService.set(this.authKey, generateToken())
 
     this.logger.log('Logined.')
   }
 
   public logout() {
     this._isLogined.set(false)
-    this.localStorageService.remove(this.authKey)
+    this.storageService.remove(this.authKey)
 
     this.logger.log('Logged out.')
   }
