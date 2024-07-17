5d0840ef65c1dd2485e292b2846578ad6d1aaddc
VKTRenokh <viktorenokh@gmail.com>
2024-07-17 11:36:23 +0300

    fix: detailed info component

--- youtube/src/app/youtube/components/video-detailed-info/video-detailed-info.component.ts
+++ youtube/src/app/youtube/components/video-detailed-info/video-detailed-info.component.ts
@@ -1,17 +1,25 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  OnInit,
   inject,
-  input,
 } from '@angular/core'
 import { SearchService } from '../../services/search/search.service'
-import { DatePipe, NgOptimizedImage } from '@angular/common'
+import {
+  AsyncPipe,
+  DatePipe,
+  NgOptimizedImage,
+} from '@angular/common'
 import { ButtonComponent } from '../../../shared/components/button/button.component'
 import { ActivatedRoute, Router } from '@angular/router'
 import { ColoredBorderDirective } from '../../directives/colored-border.directive'
 import { VideoStatisticsComponent } from '../video-statistics/video-statistics.component'
-import { filter, map, switchMap, tap } from 'rxjs'
+import {
+  Observable,
+  filter,
+  map,
+  switchMap,
+  tap,
+} from 'rxjs'
 import { isNotNullable } from '../../../shared/utils/is-not-nullable'
 import { isString } from '../../../shared/utils/is-string'
 import { SearchVideoItem } from '../../models/response.model'
@@ -25,45 +33,26 @@ import { SearchVideoItem } from '../../models/response.model'
     ColoredBorderDirective,
     DatePipe,
     VideoStatisticsComponent,
+    AsyncPipe,
   ],
   templateUrl: './video-detailed-info.component.html',
   styleUrl: './video-detailed-info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VideoDetailedInfoComponent implements OnInit {
+export class VideoDetailedInfoComponent {
   private searchService = inject(SearchService)
   private router = inject(Router)
   private route = inject(ActivatedRoute)
 
-  public id = input.required<string>()
-
-  public video: SearchVideoItem | null = null
-
-  public ngOnInit(): void {
-    this.route.params
-      .pipe(
-        map(params => params['id']),
-        filter(isString),
-        switchMap(id =>
-          this.searchService.getVideoById(id),
-        ),
-        filter(isNotNullable),
-      )
-      .subscribe(video => {
-        console.log('test', video)
-        this.video = video
-      })
-  }
+  public video: Observable<SearchVideoItem> =
+    this.route.params.pipe(
+      map(params => params['id']),
+      filter(isString),
+      switchMap(id => this.searchService.getVideoById(id)),
+      filter(isNotNullable),
+    )
 
   public navigateToTheMainPage() {
     this.router.navigate(['/'])
-  }
-
-  public getThumbnail() {
-    if (!this.video) {
-      return
-    }
-
-    return this.video.snippet.thumbnails.high
   }
 }
