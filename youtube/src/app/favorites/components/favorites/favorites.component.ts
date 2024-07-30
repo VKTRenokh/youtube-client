9b459fe7266fe81e55968b7efe3dc88f7c1e6429
VKTRenokh <viktorenokh@gmail.com>
2024-07-30 22:47:17 +0300

    feat: display favorite videos on favorites page

--- youtube/src/app/favorites/components/favorites/favorites.component.ts
+++ youtube/src/app/favorites/components/favorites/favorites.component.ts
@@ -5,23 +5,18 @@ import {
   inject,
 } from '@angular/core'
 import { Store } from '@ngrx/store'
-import {
-  selectFavoriteIds,
-  selectFavoriteVideos,
-} from '../../../state/selectors/favorite.selector'
+import { selectFavoriteVideos } from '../../../state/selectors/favorite.selector'
+import { SearchItemComponent } from '../../../youtube/components/search-item/search-item.component'
 
 @Component({
   selector: 'yt-favorites',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, SearchItemComponent],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FavoritesComponent {
   private store = inject(Store)
-  public favorites = this.store.select(selectFavoriteIds)
-  public test = this.store
-    .select(selectFavoriteVideos)
-    .subscribe(console.log)
+  public videos = this.store.select(selectFavoriteVideos)
 }
