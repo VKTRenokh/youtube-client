38d3ad60e6f10217e8122069bc550d12cdd2d3b0
VKTRenokh <viktorenokh@gmail.com>
2024-07-31 15:39:27 +0300

    refactor: remove useless class field

--- youtube/src/app/youtube/components/video-detailed-info/video-detailed-info.component.ts
+++ youtube/src/app/youtube/components/video-detailed-info/video-detailed-info.component.ts
@@ -16,7 +16,7 @@ import { VideoStatisticsComponent } from '../video-statistics/video-statistics.c
 import { Observable, filter, map, switchMap } from 'rxjs'
 import { isNotNullable } from '../../../shared/utils/is-not-nullable'
 import { isString } from '../../../shared/utils/is-string'
-import { SearchVideoItem } from '../../models/response.model'
+import { VideoItem } from '../../models/response.model'
 import { FavoriteButtonComponent } from '../../../shared/components/favorite-button/favorite-button.component'
 
 @Component({
@@ -39,15 +39,13 @@ export class VideoDetailedInfoComponent {
   private searchService = inject(SearchService)
   private router = inject(Router)
   private route = inject(ActivatedRoute)
-  public id = this.route.params.pipe(
-    map(params => params['id']),
-    filter(isString),
-  )
-
-  public video: Observable<SearchVideoItem> = this.id.pipe(
-    switchMap(id => this.searchService.getVideoById(id)),
-    filter(isNotNullable),
-  )
+  public video: Observable<VideoItem> =
+    this.route.params.pipe(
+      map(params => params['id']),
+      filter(isString),
+      switchMap(id => this.searchService.getVideoById(id)),
+      filter(isNotNullable),
+    )
 
   public navigateToTheMainPage() {
     this.router.navigate(['/'])
