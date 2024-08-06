29905d52f86f7538b22c9bc03bce32b9bd47cbd7
VKTRenokh <viktorenokh@gmail.com>
2024-08-06 22:10:15 +0300

    feat: add get storage service test

--- youtube/src/app/core/services/storage/storage.service.spec.ts
+++ youtube/src/app/core/services/storage/storage.service.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { LocalStorageService } from './local-storage.service';
+import { StorageService } from './storage.service'
 
 describe('LocalStorageService', () => {
-  let service: LocalStorageService;
+  let service: StorageService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LocalStorageService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(StorageService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+
+  it('should save things', () => {
+    const key = 'alalalallala'
+    const value = '10'
+
+    expect(service.get(key)).toBeUndefined()
+
+    service.set(key, value)
+
+    expect(service.get(key)).toBe(value)
+  })
+})
