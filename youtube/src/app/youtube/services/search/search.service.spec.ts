82e419c998704a8fa3d5db097a70effca46f1380
VKTRenokh <viktorenokh@gmail.com>
2024-08-06 18:07:30 +0300

    fix: incorrect configuration was used

--- youtube/src/app/youtube/services/search/search.service.spec.ts
+++ youtube/src/app/youtube/services/search/search.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing'
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing'
 
 import { SearchService } from './search.service'
+import { provideHttpClient } from '@angular/common/http'
 
 describe('SearchService', () => {
   let service: SearchService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    })
     service = TestBed.inject(SearchService)
   })
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should search', () => {
+    const testingController = TestBed.inject(
+      HttpTestingController,
+    )
+
+    service.search('How to test angular in depth')
+
+    const req = testingController.expectOne(
+      '/search',
+      'Request to search videos',
+    )
+
+    req.flush([])
   })
 })
