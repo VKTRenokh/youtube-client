868192aee9d4eedd31a61326ff85928af9fc923c
VKTRenokh <viktorenokh@gmail.com>
2024-08-06 16:51:49 +0300

    feat: add test case for filtering service

--- youtube/src/app/youtube/services/filter/filter.service.spec.ts
+++ youtube/src/app/youtube/services/filter/filter.service.spec.ts
@@ -1,16 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { FilterService } from './filter.service';
+import { FilterService } from './filter.service'
 
 describe('FilterService', () => {
-  let service: FilterService;
+  let service: FilterService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(FilterService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(FilterService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+
+  it('signal value should be changed when toggleIsFilteringShown called', () => {
+    const isFilteringShown = service.getIsFilteringShown()
+
+    expect(isFilteringShown()).toBe(false)
+
+    service.toggleIsFilteringShown()
+
+    expect(isFilteringShown()).toBe(true)
+  })
+})
