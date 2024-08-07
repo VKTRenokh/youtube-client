38da8d8a2b68a3fe9dc15b05f3af14b5706c9a65
VKTRenokh <viktorenokh@gmail.com>
2024-08-07 15:00:05 +0300

    fix: logger wasn't provided

--- youtube/src/app/auth/services/auth/auth.service.spec.ts
+++ youtube/src/app/auth/services/auth/auth.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing'
 
 import { AuthService } from './auth.service'
+import { provideLogger } from '../../../core/providers/logger/logger.provider'
 
 describe('AuthService', () => {
   let service: AuthService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      providers: [provideLogger()],
+    })
     service = TestBed.inject(AuthService)
   })
 
--- youtube/src/app/youtube/services/videos-http/videos-http.service.spec.ts
+++ youtube/src/app/youtube/services/videos-http/videos-http.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing'
 
 import { VideosHttpService } from './videos-http.service'
+import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClientTesting } from '@angular/common/http/testing'
 
 describe('VideosHttpService', () => {
   let service: VideosHttpService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    })
+
     service = TestBed.inject(VideosHttpService)
   })
 
