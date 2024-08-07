00cdfc00dc2701cb1f9a65497b4a61a2868fc110
VKTRenokh <viktorenokh@gmail.com>
2024-08-07 18:38:38 +0300

    fix: test from custom-search-item

--- youtube/src/app/youtube/components/custom-search-item/custom-search-item.component.spec.ts
+++ youtube/src/app/youtube/components/custom-search-item/custom-search-item.component.spec.ts
@@ -4,6 +4,12 @@ import {
 } from '@angular/core/testing'
 
 import { CustomSearchItemComponent } from './custom-search-item.component'
+import { provideMockStore } from '@ngrx/store/testing'
+import { InputSignal, signal } from '@angular/core'
+import { CustomCard } from '../../../admin/models/custom-card.model'
+
+const inputSignal = <T>(value: T) =>
+  signal(value) as unknown as InputSignal<T>
 
 describe('CustomSearchItemComponent', () => {
   let component: CustomSearchItemComponent
@@ -12,12 +18,23 @@ describe('CustomSearchItemComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CustomSearchItemComponent],
+      providers: [provideMockStore({})],
     }).compileComponents()
 
     fixture = TestBed.createComponent(
       CustomSearchItemComponent,
     )
     component = fixture.componentInstance
+    component.item = inputSignal<CustomCard>({
+      id: 'id',
+      tags: ['tag'],
+      title: 'Title',
+      isCustom: true,
+      createdAt: new Date().toString(),
+      imageLink: 'https://github.com',
+      videoLink: 'https://github.com',
+      description: 'Aalalalala',
+    })
     fixture.detectChanges()
   })
 
