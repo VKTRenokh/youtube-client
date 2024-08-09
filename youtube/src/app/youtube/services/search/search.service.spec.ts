75d3af5c69330954e54cb73ccf0b2c5e89d1e6bd
VKTRenokh <viktorenokh@gmail.com>
2024-08-09 17:44:31 +0300

    feat: add test for getVideoById

--- youtube/src/app/youtube/services/search/search.service.spec.ts
+++ youtube/src/app/youtube/services/search/search.service.spec.ts
@@ -7,6 +7,7 @@ import {
 import { SearchService } from './search.service'
 import { provideHttpClient } from '@angular/common/http'
 import { firstValueFrom } from 'rxjs'
+import { isGet } from '../../../utils/is-get'
 
 describe('SearchService', () => {
   let service: SearchService
@@ -23,6 +24,30 @@ describe('SearchService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should correctly get video by id', async () => {
+    const id = 'id2135'
+
+    const testingController = TestBed.inject(
+      HttpTestingController,
+    )
+
+    const video = firstValueFrom(service.getVideoById(id))
+
+    const req = testingController.expectOne(
+      req =>
+        req.url === '/videos' &&
+        req.params.has('id') &&
+        isGet(req),
+    )
+
+    req.flush({ items: [{ id }, { id: '134' }] })
+
+    const response = await video
+
+    expect(response).toBeTruthy()
+    expect(response!.id).toBe(id)
   })
 
   it('should search', async () => {
@@ -49,7 +74,8 @@ describe('SearchService', () => {
     const videosReq = testingController.expectOne(
       req =>
         req.url === '/videos' &&
-        req.params.get('id') === videoId,
+        req.params.get('id') === videoId &&
+        isGet(req),
     )
 
     videosReq.flush({
