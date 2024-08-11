41ef3a09ad91d51bddc4d1da8d6f32fa3c64f6c2
VKTRenokh <viktorenokh@gmail.com>
2024-08-11 17:58:24 +0300

    feat: add select favorite videos selector test

--- youtube/src/app/state/selectors/favorite.selector.spec.ts
+++ youtube/src/app/state/selectors/favorite.selector.spec.ts
@@ -1,12 +1,72 @@
+import { VideoItem } from '../../youtube/models/response.model'
 import { AppState } from '../types/app-state.type'
-import { selectFavoriteIds } from './favorite.selector'
+import {
+  selectFavoriteIds,
+  selectFavoriteVideos,
+} from './favorite.selector'
 
 describe('Favorite selectors', () => {
-  const favoriteIds = ['bimbom']
+  const id = 'bimbom'
+  const favoriteIds = [id]
+
+  const card: VideoItem = {
+    id,
+    etag: 'asfdas',
+    kind: 'asfds',
+    snippet: {
+      tags: ['asf'],
+      title: 'adfasf',
+      channelId: 'adfasdf',
+      localized: {
+        title: 'adfasdf',
+        description: 'afasdfjlk',
+      },
+      description: 'adf',
+      categoryId: 'sdfasdfs',
+      thumbnails: {
+        maxres: {
+          url: 'asfsadf',
+          width: 22,
+          height: 2,
+        },
+        high: {
+          url: 'asfsadf',
+          width: 22,
+          height: 2,
+        },
+        medium: {
+          url: 'asfsadf',
+          width: 22,
+          height: 2,
+        },
+        default: {
+          url: 'asfsadf',
+          width: 22,
+          height: 2,
+        },
+        standard: {
+          url: 'asfsadf',
+          width: 22,
+          height: 2,
+        },
+      },
+      publishedAt: new Date().toString(),
+      channelTitle: 'adfasdf',
+      defaultAudioLanguage: 'adsfsad',
+      liveBroadcastContent: 'adfsadf',
+    },
+    statistics: {
+      likeCount: '32432',
+      viewCount: 'dsafasdf',
+      commentCount: 'dfолвафыва',
+      dislikeCount: 'adfsadf',
+      favoriteCount: 'sdfasdf',
+    },
+  }
 
   const initialState: AppState = {
     youtube: {
-      data: [],
+      data: [card],
       loading: false,
       customCards: [],
       error: null,
@@ -22,5 +82,15 @@ describe('Favorite selectors', () => {
     expect(
       selectFavoriteIds.projector(initialState.favorites),
     ).toBe(favoriteIds)
+  })
+
+  it('should select favorite vidoes', () => {
+    const result = selectFavoriteVideos.projector(
+      initialState.favorites.data,
+      initialState.youtube.data,
+    )
+
+    expect(result).toBeTruthy()
+    expect(result![0]).toBe(card)
   })
 })
