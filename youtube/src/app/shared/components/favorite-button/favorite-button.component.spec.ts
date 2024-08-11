4bde310eeded1ca2ceef2934c01cc637ae1ca40f
VKTRenokh <viktorenokh@gmail.com>
2024-08-11 13:54:46 +0300

    feat: add favorite button tests

--- youtube/src/app/shared/components/favorite-button/favorite-button.component.spec.ts
+++ youtube/src/app/shared/components/favorite-button/favorite-button.component.spec.ts
@@ -4,15 +4,23 @@ import {
 } from '@angular/core/testing'
 
 import { FavoriteButtonComponent } from './favorite-button.component'
-import { provideMockStore } from '@ngrx/store/testing'
-import { inputSignal } from '../../../utils/input-signal'
+import {
+  MockStore,
+  provideMockStore,
+} from '@ngrx/store/testing'
+import { By } from '@angular/platform-browser'
+import { DebugElement } from '@angular/core'
+import { firstValueFrom } from 'rxjs'
+import { FavoriteActions } from '../../../state/actions/favorite.actions'
 
 describe('FavoriteButtonComponent', () => {
   const id = 'daedalus37'
   const initialState = { favorites: { data: [id] } }
+  const initialStateWithoutId = { favorites: { data: [] } }
 
   let component: FavoriteButtonComponent
   let fixture: ComponentFixture<FavoriteButtonComponent>
+  let store: MockStore
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,11 +36,38 @@ describe('FavoriteButtonComponent', () => {
       FavoriteButtonComponent,
     )
     component = fixture.componentInstance
-    component.id = inputSignal(id)
+    fixture.componentRef.setInput('id', id)
     fixture.detectChanges()
+
+    store = TestBed.inject(MockStore)
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should be favorite if has id in store', () => {
+    expect(component.isFavorite()).toBeTruthy()
+  })
+
+  it('should dispatch correct action when button is clicked', async () => {
+    const spy = jest.spyOn(store, 'dispatch')
+    const button: DebugElement = fixture.debugElement.query(
+      By.css('.favorite-button button'),
+    )
+
+    button.triggerEventHandler('click')
+
+    expect(spy).toHaveBeenCalledWith(
+      FavoriteActions.remove({ id }),
+    )
+
+    spy.mockReset()
+    store.setState(initialStateWithoutId)
+    button.triggerEventHandler('click')
+
+    expect(spy).toHaveBeenCalledWith(
+      FavoriteActions.add({ id }),
+    )
   })
 })
