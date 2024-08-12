fb7b76512a4175636f98c22c7143e6ae2bf49436
VKTRenokh <viktorenokh@gmail.com>
2024-08-12 08:44:16 +0300

    feat: add test for FavoriteActions.remove

--- youtube/src/app/state/reducers/favorites.reducer.spec.ts
+++ youtube/src/app/state/reducers/favorites.reducer.spec.ts
@@ -5,6 +5,8 @@ import {
 } from './favorites.reducer'
 
 describe('Favorites Reducer', () => {
+  const id = 'pipka'
+
   it('should return the default state', () => {
     expect(
       favoritesReducer(initialState, {
@@ -14,12 +16,26 @@ describe('Favorites Reducer', () => {
   })
 
   it('should add new id to the store', () => {
-    const id = 'pipka'
     expect(
       favoritesReducer(
         initialState,
         FavoriteActions.add({ id: id }),
       ).data.includes(id),
     ).toBeTruthy()
+  })
+
+  it('should remove newly added id from the store', () => {
+    const newState = favoritesReducer(
+      initialState,
+      FavoriteActions.add({ id }),
+    )
+    expect(newState.data.includes(id)).toBeTruthy()
+
+    expect(
+      favoritesReducer(
+        newState,
+        FavoriteActions.remove({ id }),
+      ).data.includes(id),
+    ).toBeFalsy()
   })
 })
